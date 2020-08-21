ea29763b46b156d9d4f0c521ddb46a0f04ec895c
Axedez <45712870+Axedez@users.noreply.github.com>
2020-08-21 19:26:06 +0300

    Work with favicon and responsive

--- js/main.js
+++ js/main.js
@@ -7,3 +7,8 @@ var mySwiper = new Swiper('.swiper-container', {
     clickable: true,
   },
 });
+if (document.body.clientWidth <= 835) {
+  $(function () {
+    $('.hero-avatar').attr('data-wow-delay', '0.5s');
+  });
+}
